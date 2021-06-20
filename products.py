bb5e73c873d0b270534a20985854fd623d1ef86a
michael wu <michael10366@outlook.com>
2021-06-20 20:37:26 +0800

    add read files

--- products.py
+++ products.py
@@ -1,7 +1,6 @@
 #products
 
 #讀取檔案
-
 products = []
 
 with open('products.csv', 'r', encoding = 'UTF-8') as f:
@@ -13,7 +12,6 @@ with open('products.csv', 'r', encoding = 'UTF-8') as f:
 print(products)
 
 #使用者書輸入		
-
 while True:
 	name = input('請輸入商品: ')
 	
@@ -24,12 +22,10 @@ while True:
 	products.append([name, price])
 
 #印出商品
-
 for p in products:
 	print(p[0], '的價格是', p[1])
-	
-#寫入檔案
 
+#寫入檔案
 with open('products.csv', 'w', encoding = 'UTF-8') as f:
 	f.write('商品,價格\n')
 	for p in products:

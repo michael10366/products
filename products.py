9633f2baa66a09c5187c98dfb2e653b84e47308b
michael wu <michael10366@outlook.com>
2021-06-20 20:32:38 +0800

    add read files

--- products.py
+++ products.py
@@ -1,19 +1,27 @@
 #products
-
 products = []
-while True:
-	name = input('請輸入商品: ')
+
+with open('products.csv', 'r', encoding = 'UTF-8') as f:
+	for line in f:
+		if '商品,價格' in line:
+			continue
+		name, price = line.strip().split(',')
+		products.append([name, price])
+print(products)
+		
+# while True:
+# 	name = input('請輸入商品: ')
 	
-	if name == "q": #quit
-		break
-	price = input('請輸入價格: ')
+# 	if name == "q": #quit
+# 		break
+# 	price = input('請輸入價格: ')
 
-	products.append([name, price])
+# 	products.append([name, price])
 
-for p in products:
-	print(p[0], '的價格是', p[1])
+# for p in products:
+# 	print(p[0], '的價格是', p[1])
 
-with open('products.csv', 'w', encoding = 'UTF-8') as f:
-	f.write('商品,價格\n')
-	for p in products:
-		f.write(p[0] + ',' + p[1] + '\n')
+# with open('products.csv', 'w', encoding = 'UTF-8') as f:
+# 	f.write('商品,價格\n')
+# 	for p in products:
+# 		f.write(p[0] + ',' + p[1] + '\n')
